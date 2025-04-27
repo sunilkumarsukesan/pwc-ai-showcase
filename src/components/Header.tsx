e542d81088927dab24cef388d52d7c394a8307e2
sunilkumarsukesan <37512791+sunilkumarsukesan@users.noreply.github.com>
2025-04-27 17:12:56 +0530

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
   <div className="max-w-7xl mx-auto flex justify-between items-center relative">
     {/* Logo aligned left */}
     <div className="absolute left-0 flex items-center">
-    <img src="/logo.png" alt="PwC Logo" className="w-20 h-12 mr-2" />
+    <img src="/pwc-ai-showcase/logo.png" alt="PwC Logo" className="w-20 h-12 mr-2" />
     </div>
 
     {/* Centered Title */}
